8992736175a9ad05767f55c51a863a3f093379ae
Vladimir Stsiatsko <vladimir.stsiatsko@intexsoft.by>
2018-05-01 14:45:08 +0300

    deleted .idea

---
+++ spec/spec1.e2e-spec.js
@@ -0,0 +1,29 @@
+describe('angularjs homepage', function() {
+    it('should greet the named user', function() {
+        // Load the AngularJS homepage.
+        browser.get('http://www.angularjs.org');
+
+        // Find the element with ng-model matching 'yourName' - this will
+        // find the <input type="text" ng-model="yourName"/> element - and then
+        // type 'Julie' into it.
+        var nameElement = element(by.model('yourName'));
+        nameElement.sendKeys('Julie');
+
+        // Find the element with binding matching 'yourName' - this will
+        // find the <h1>Hello {{yourName}}!</h1> element.
+        var greeting = element(by.binding('yourName'));
+
+        // Assert that the text element has the expected value.
+        // Protractor patches 'expect' to understand promises.
+        expect(greeting.getText()).toEqual('Hello Julie!');
+    });
+
+    it('should use search', function () {
+        browser.get('https://angular.io/docs');
+        var searchFieldElement = $('[aria-label="search"]');
+        searchFieldElement.sendKeys('Protractor');
+        searchFieldElement.sendKeys(protractor.Key.ENTER);
+        browser.sleep(1000); // только для демонстрации
+        expect($('.search-results').isDisplayed()).toBe(true);
+    });
+});
