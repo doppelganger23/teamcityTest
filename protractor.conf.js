65b4a44dd4a5f5a6df38606baf41df946956c24d
Vladimir Stsiatsko <vladimir.stsiatsko@intexsoft.by>
2018-05-08 18:53:37 +0300

    подключил allure

--- protractor.conf.js
+++ protractor.conf.js
@@ -15,5 +15,12 @@ exports.config = {
     // Options to be passed to Jasmine-node.
     jasmineNodeOpts: {
         showColors: true // Use colors in the command line report.
+    },
+
+    onPrepare: function() {
+        var AllureReporter = require('jasmine-allure-reporter');
+        jasmine.getEnv().addReporter(new AllureReporter({
+            resultsDir: 'allure-results'
+        }));
     }
 };
